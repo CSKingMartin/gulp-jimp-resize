17376de07ebaec92d4ae6840dabb125c3df08e85
CSKingMartin <cwells@linfield.edu>
2016-07-05 16:08:17 -0700

    not quite gulpy, but does something. async errors still

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,9 @@ var debug = require('gulp-debug');
 
 
 gulp.task('images', function() {
-	gulp.src('./test/**.*')
+	gulp.src('./test/*.jpg')
 	.pipe(debug())
-	.pipe(jimp(['lg', 'md']))
-	.pipe(gulp.dest('resized'));
+	.pipe(jimp(['lg', 'md', 'squareV-sm', 'sm']))
+	.pipe(gulp.dest('./resized/'));
 });
-		
 		
