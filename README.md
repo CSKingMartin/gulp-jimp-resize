30ab0c59dfb5cdef5e5fd03fbba3281ce72a8717
CSKingMartin <cwells@linfield.edu>
2016-07-13 16:26:02 -0700

    base dev release -- no cropping

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # gulp-jimp-resize [![Build Status](https://travis-ci.org/CSKingMartin/gulp-jimp-resize.svg?branch=master)](https://travis-ci.org/CSKingMartin/gulp-jimp-resize)
 Maniuplate images' size with few dependencies. Uses JIMP wrapped with gulp.
 
-Inspired by the same functionality as gulp-jimp, which has open issues crucial to it's functionality. JIMP is a great, simple image processing library with 0 dependencies. This gulp plugin wraps JIMP functionality for gulp in an extremely simple way purely for resizing images ONLY. Resizes based on one dimension passed while keeping the aspect ratio of the original image, or can crop to a square image.
+Inspired by the same functionality as gulp-jimp, which has open issues crucial to it's functionality. JIMP is a great, simple image processing library with 0 dependencies. This gulp plugin wraps JIMP functionality for gulp in an extremely simple way for resizing images.
 
 # install:
 ```
@@ -9,7 +9,7 @@ npm install --save-dev gulp-jimp-resize
 ```
 
 # how to:
-Usage is pretty simple. Pipe in a directory of images to the "gulp-jimp-resize" module, and then pipe it somewhere else as you wish (for minification, or to a new directory, etc). Each image will be resized to EACH option piped into the plugin. The example gulpfile below will produce 5 new images for every image (custom, md, sm, square, and custom2).
+Usage is pretty simple. Pipe in a directory of images to the "gulp-jimp-resize" module, and then pipe it somewhere else as you wish (for minification, or to a new directory, etc). Each image will be resized to EACH option piped into the plugin. The example gulpfile below will produce 3 new images for every image piped.
 
 # example gulp file:
 ```js
@@ -17,74 +17,22 @@ var jimp = require("gulp-jimp-resize");
 var gulp = require('gulp');
 
 gulp.task('images', function() {
-return gulp.src('./originals/**/*.*')
-	.pipe(jimp([
-		{"suffix": "-custom", "dimension": 1000, "square": true}, //custom entry
-	
-		'md', 'sm', 'square', //examples of defaults
-	
-		{"suffix": "-custom2", "dimension": 500, "square": false}]))
+	return gulp.src('./originals/**/*.*')
+	.pipe(jimp({
+		sizes: [
+			{"suffix": "md", "width": 960},
+			{"suffix": "sm", "height": 480},
+			{"width": 800, "height": 720},
+			{"suffix": "original"}
+		]
+	}))
 	.pipe(gulp.dest('./resized/'));
-
-	.pipe(jimp({
-		prefixAll: '',
-		sizes: [
-			{ suffix: '', width: 100, square: false },
-			{ suffix: '', height: 100, square: false }
-		]
-	}))
-
-	.pipe(jimp({
-		sizes: [
-		    {  },
-			jimp.default.xl,
-			jimp.default.md,
-		]
-	}))
-});
+});	
 		
 ```
 
 #options:
-gulp-jimp-resize supports custom options, and has structure for creation of defaults for quick access. The current defaults are located in the 'index.js' file of the module. These can be edited if desired. Each option passed has 3 parameters: the suffix -- which will be included in the name of resized image; the dimension -- which will become the size of the LARGEST side of the new resized image (this is to keep the aspect ratio intact); and the square property -- which will crop your image to a square whose dimensions are both equal to the "dimension" property.
-
-The current defaults (located in the index.js folder) are placed below:
-```js
-
-var jimp = require('gulp-jimp-resize');
-jimp.default =
-	{
-		"xl": {
-			"width": 1500,
-			"suffix": "-xl",
-			"crop": false
-		"lg": {
-			"width": 1220,
-			"suffix": "-lg",
-			"crop": false
-		"md": {
-			"width": 960,
-			"suffix": "-md",
-			"crop": false
-		"sm": {
-			"width": 480,
-			"suffix": "-sm",
-			"crop": false
-		"xs": {
-			"width": 320,
-			"suffix": "-xs",
-			"crop": false
-		"square": {
-			"width": 500,
-			"suffix": "-square",
-			"crop": true
-		"square-sm": {
-			"width": 320,
-			"suffix": "-square-sm",
-			"crop": true
-	}
-```
-
+gulp-jimp-resize takes an Object 'sizes'. Each element inside 'sizes' is produced from each image passed to the plugin. Each option has 3 optional attributes. Suffix denotes the added string to the name of the new image. Width and height are self explainitory. If only one of width or height is passed through, the plugin will keep the apsect ratio of the original image. These attributes are also optional (i.e, you can give a suffix only and rename the image).
 
 #Licence
 MIT
--- index.js
+++ index.js
@@ -6,75 +6,9 @@ var PluginError = gutil.PluginError;
 
 const PLUGIN_NAME = "gulp-jimp-resize";
 
-var defaults =
-	{
-		"xl": {
-			"dimension": 1500,
-			"suffix": "-xl",
-			"square": false
-			},
-		"lg": {
-			"dimension": 1220,
-			"suffix": "-lg",
-			"square": false
-			},
-		"md": {
-			"dimension": 960,
-			"suffix": "-md",
-			"square": false
-			},
-		"sm": {
-			"dimension": 480,
-			"suffix": "-sm",
-			"square": false
-			},
-		"xs": {
-			"dimension": 320,
-			"suffix": "-xs",
-			"square": false
-			},
-		"square": {
-			"dimension": 500,
-			"suffix": "-square",
-			"square": true
-			},
-		"square-sm": {
-			"dimension": 320,
-			"suffix": "-square-sm",
-			"square": true
-			},
-	}
-
 var newImages = [];
 
-function goGoGadgetImageResize (file, opt) {
-
-	//console.log(option)
-
-	if(typeof opt == 'object'){
-		var option = opt;
-	} else {
-		var option = defaults[opt];
-	}
-
-	if(!option) {
-		throw new PluginError(PLUGIN_NAME, "Default option doesn't exist");
-	}
-
-	var dim = option.dimension;
-	var suffix = option.suffix;
-	var crop = option.square;
-	var path = file.path;
-
-	//console.log(dim, suffix, crop);
-
-
-	if(!dim || !suffix || crop == null){
-		throw new PluginError(PLUGIN_NAME, "Custom option missing metadata");
-	}
-
-	var name = path.substring(path.lastIndexOf('\\')+1, path.lastIndexOf('.')) + suffix + ".png";
-
+function goGoGadgetImageCrop (file, opt) {
 	return new Promise(function(resolve, reject) {
 		Jimp.read(file.contents, function (err, image) {
 			if (err) {
@@ -82,34 +16,59 @@ function goGoGadgetImageResize (file, opt) {
 				return;
 			}
 
-	        var center = 0;
+			var center = 0;
 
-	        //RESIZE
-        	if(image.bitmap.width > image.bitmap.height) { //horizontal image
-        		if(option == 'square' || 'square-sm'){
-        			//square crop we crop to different dimensions
-        			image.resize(Jimp.AUTO, dim);
-        		} else {
-        			image.resize(dim, Jimp.AUTO)
-        		}
-        		image.quality(100);
-        		var center = (image.bitmap.width - dim) / 2;
-        	} 
-        	else { 		// vertical image
-        	 	if(option == 'square' || 'square-sm'){
-        			image.resize(dim, Jimp.AUTO);
-        		} else {
-        			image.resize(Jimp.AUTO, dim)
-        		}
-		        image.quality(100);
-        	}
-
-       		//CROP
+			//CROP
     		if(crop == true) {
-        		
         		image.crop(center, 0, dim, dim);
         	}
-    	
+		})
+	})
+};
+
+function goGoGadgetImageResize (file, opt) {
+
+	if(opt.width && opt.height && opt.preserveAspect == true){
+		throw new PluginError(PLUGIN_NAME, 'You cannot preserve the aspect ratio while resizing the height and width');
+	};
+
+	var path = file.path;
+
+	var suffix = "";
+
+	if(opt.suffix) {
+		var suffix = "-" + opt.suffix;
+	}
+	
+	var name = path.substring(path.lastIndexOf('\\')+1, path.lastIndexOf('.')) + suffix + ".png";
+
+	if(!opt.width && !opt.height) { //no resizing to be done
+		return new gulp-util.File({
+			path: name,
+			contents: file.contents
+		})
+	}
+
+	var result = new Promise(function(resolve, reject) {
+		Jimp.read(file.contents, function (err, image) {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			var width = Jimp.AUTO; var height = Jimp.AUTO;
+
+			if((opt.width && opt.width > image.bitmap.width) || 
+				(opt.height && opt.height > image.bitmap.height)) {
+				gutil.log(PLUGIN_NAME, 'You are resizing an image to a larger size than the original');
+			}
+
+			if(opt.width) { width = opt.width; }
+
+			if(opt.height) { height = opt.height; }
+			
+			image.resize(width, height).quality(100);
+        	
 	       	var newImg = image.getBuffer(Jimp.MIME_JPEG, function(err, buffer){
 				if (err) {
 					reject(err);
@@ -123,30 +82,29 @@ function goGoGadgetImageResize (file, opt) {
 	        });
 		});
 	})
+
+	//crop here or separate?
+
+	return result;
 };
+
 
 function gulpJimpResize(options) {
 	if (!options) {
 		throw new PluginError(PLUGIN_NAME, 'Missing options entry!');
 	}
-
-	//console.log(this);
-
 	return through.obj(function(file, enc, cb) { //transform function
 		if(!file){
 			throw new PluginError(PLUGIN_NAME, 'No files passed!');
 			return cb(null, file);
 		}
 
-		Promise.all(options.map(opt => goGoGadgetImageResize(file, opt)))
+		Promise.all(options.sizes.map(opt => goGoGadgetImageResize(file, opt)))
 			.then(imageArray => {
-				imageArray.forEach(image => this.push(image))
-
+				imageArray.forEach(image => this.push(image)) 
 				cb();
 			});
 	});
 }
 
 module.exports = gulpJimpResize;
-
-
